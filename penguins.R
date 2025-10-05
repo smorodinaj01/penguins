a0f1c850a686c8dea9f2b82657417a40a3301fca
Zhanna Smorodina <smorodina.zhv@phystech.edu>
2025-10-05 12:09:11 +0400

    add x naming

--- penguins.R
+++ penguins.R
@@ -13,5 +13,5 @@ data <- read.csv("penguins.csv")
 x <- data$bill_length_mm
 
 png("penguins-hist.png")
-hist(x, breaks = seq(40, 60, 2))
+hist(x, breaks = seq(40, 60, 2), xlab = "Bill length, mm")
 dev.off()
