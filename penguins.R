12591c1b44725b49b92f19f6d96628568146a60d
Zhanna Smorodina <smorodina.zhv@phystech.edu>
2025-10-05 12:27:43 +0400

    add comment

--- penguins.R
+++ penguins.R
@@ -12,6 +12,8 @@ hist(x, breaks = seq(40, 60, 1))
 data <- read.csv("penguins.csv")
 x <- data$bill_length_mm
 
+# Ящик с усами для длины клюва
+
 png("penguins-boxplot.png")
 boxplot(x, ylab = "Bill length, mm")
 dev.off()
